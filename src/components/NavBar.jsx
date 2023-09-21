c0f2814166a014605dcbf9df11434420a8c2c5c5
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-21 19:18:18 +0500

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "../data";
+import logo from "../logo.svg";
+
+const NavBar = () => {
+  return <h4>navbar</h4>;
+};
+
+export default NavBar;
